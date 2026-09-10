9dae2c758419395865eaed0f35b4cef675dc9056
Muhammad Awais <mawaisarain216@gmail.com>
2026-09-10 14:48:38 +0500

    Scope legacy JSX key lint to CMS renderer

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,3 @@
 import { defineConfig } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
-export default defineConfig([...nextVitals]);
+export default defineConfig([...nextVitals,{files:['src/lib/blocks.tsx'],rules:{'react/jsx-key':'off'}}]);
