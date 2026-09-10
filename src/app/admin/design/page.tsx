bfdeadf75c1e3798408dc278b6c54e81d21dff0f
Muhammad Awais <mawaisarain216@gmail.com>
2026-09-10 18:12:45 +0500

    Remove premature admin design studio

--- src/app/admin/design/page.tsx
+++
@@ -1,7 +0,0 @@
-import WebsiteDesignStudio from '@/components/admin/WebsiteDesignStudio'
-import { getSiteDesign } from '@/lib/site-design'
-
-export default async function DesignSystem() {
-  const design = await getSiteDesign()
-  return <WebsiteDesignStudio initial={design} />
-}
